8ee98f404f4bda3f32b37024ef76dd0c91e65e63
J0nas <vit0r@users.noreply.github.com>
2022-11-14 16:53:40 -0300

    Update Install-Containerd.ps1
    fix param

--- kubeadm/scripts/Install-Containerd.ps1
+++ kubeadm/scripts/Install-Containerd.ps1
@@ -24,8 +24,8 @@ PS> .\Install-Conatinerd.ps1
 Param(
     [parameter(HelpMessage = "ContainerD version to use")]
     [string] $ContainerDVersion = "1.5.9",
-    [parameter(HelpMessage = "Interface name"]
-    [string] $netAdapterName = (Get-NetIPAddress -AddressFamily "IPv4" -InterfaceAlias "Ethernet*").InterfaceAlias
+    [parameter(HelpMessage = "Interface name")]
+    [string] $netAdapterName
 )
 
 $ErrorActionPreference = 'Stop'
